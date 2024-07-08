d415ce4033faa6f6d99e750bc3537b2278bd2adf
Youssef1ragab <145896473+Youssef1ragab@users.noreply.github.com>
2024-07-08 19:58:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//calculator app
+
+let display =document.getElementById("display");
+
+function appendToDisplay(input){
+    if(display.value==='Error')
+    {
+        clearDisplay();
+    }
+display.value+=input;
+}
+
+function clearDisplay()
+{
+    display.value = '';
+}
+
+function calculate()
+{
+    try{
+display.value=eval(display.value);
+    }
+    catch(error)
+    {
+        display.value="Error";
+    }
+}
